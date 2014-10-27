541b8898c1f052e5525f2fe873f06c5d4fc9634d
ckmis93 <ckmis93@gmail.com>
2014-10-27 18:33:51 +0100

    Update playground.php

--- playground.php
+++ playground.php
@@ -7,10 +7,10 @@
 
 include("car.php");
 
-$myCar[0] = new Car('Trabant', 70);
+$myCar[0] = new Car('Opel', 70);
 $myCar[1] = new Car('Mercedes', 150);
-$myCar[2] = new Car('Ford', 120);
-$myCar[3] = new Car('Audi', 180);
+$myCar[2] = new Car('Fiat', 120);
+$myCar[3] = new Car('Maseratti', 180);
 $myCar[4] = new Car('Ferrari', 250);
 
 for ($i=0; $i<sizeOf($myCar); $i++){
